d008f4e17c0e6cab8ea7a4e91b2b01b446af9008
nicoxeye <nicoxeye@op.pl>
2025-01-17 13:36:48 +0100

    functions in api_requests
    get_similar_artists, get_top_album_image, get_top_album_name

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-from src.api_requests import get_band_info
-from dotenv import load_dotenv
-import os
 from src.gui import start_gui
 
-def main():
-    # FOR TESTING AND QUICK REVIEWS
-    load_dotenv()
-    api_key = os.getenv('API_KEY')
-
-    if not api_key:
-        print("Error: API key not found. Please set your API key in the .env file.")
-        exit(1)
-
-    #artist_name = input("Enter the name of the band: ")
-    with open('src/bands.txt', 'r') as file:
-        bands = [line.strip() for line in file]
-
-        for artist_name in bands:
-            band_info = get_band_info(artist_name, api_key)
-            #print(band_info)
-            #if band_info:
-                #print(band_info['artist']['name'] + " - " + band_info['artist']['tags']['tag'][0]['name'])
-
-if __name__ == "__main__":
-    start_gui()
-    #main()
+start_gui()
--- src/api_requests.py
+++ src/api_requests.py
@@ -2,28 +2,10 @@ import requests
 from dotenv import load_dotenv
 import os
 
-def get_band_info(artist_name, api_key):
-    url = "http://ws.audioscrobbler.com/2.0/" #endpoint API Last.fm
-    params = {
-        "method": "artist.getinfo",
-        "artist": artist_name,
-        "api_key": api_key,
-        "format": "json"
-    }
-
-    response = requests.get(url, params=params)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        print(f"Error: Unable to fetch data; status code {response.status_code}")
-        return None
-    
 load_dotenv()
 api_key = os.getenv('API_KEY')
 
-def get_band_info1(band_name):
+def get_band_info(band_name):
     url = f"http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist={band_name}&api_key={api_key}&format=json"
     response = requests.get(url)
     return response.json() if response.status_code == 200 else None
@@ -37,3 +19,35 @@ def get_track_info(band_name):
     url = f"http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist={band_name}&api_key={api_key}&format=json"
     response = requests.get(url)
     return response.json() if response.status_code == 200 else None
+
+def get_similar_artists(band_name):
+    url = f"http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist={band_name}&api_key={api_key}&format=json"
+    response = requests.get(url)
+    return response.json() if response.status_code == 200 else None
+
+def get_top_album_image(band_name):
+    params = {
+        'method': 'artist.getTopAlbums',
+        'artist': band_name,
+        'api_key': api_key,
+        'format': 'json',
+        'limit': 1 
+    }
+    response = requests.get("http://ws.audioscrobbler.com/2.0/", params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        if 'topalbums' in data and data['topalbums']['album']:
+            album = data['topalbums']['album'][0]
+            images = album['image']
+            for img in images:
+                if img['size'] == 'large':
+                    return img['#text']
+    return None
+
+def get_top_album_name(band_name):
+    album_info = get_album_info(band_name)
+    if album_info and 'topalbums' in album_info and 'album' in album_info['topalbums']:
+        first_album = album_info['topalbums']['album'][0]
+        return first_album['name']
+    return "No albums found"
